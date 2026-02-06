48d784d11c172d4832105db188997baf8bc23ac0
sathwik0529 <sathwikdu123@gmail.com>
2026-02-06 16:07:50 +0530

    Add files via upload

---
+++ day3/day3task1.py
@@ -0,0 +1,6 @@
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+
+print("First reading:", temperatures[0])
+print("Last reading:", temperatures[-1])
+print("Afternoon Peak readings:", temperatures[3:6])
+print("Last 3 hours readings:", temperatures[-3:])
