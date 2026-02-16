bf1d7d37dbe1241707103bf904179774b08b358d
sathwik0529 <sathwikdu123@gmail.com>
2026-02-16 15:14:30 +0530

    Add files via upload

---
+++ Day11/day11_Trendtracker.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+months = [1, 2, 3, 4, 5]
+revenue = [2000, 4500, 4000, 7500, 9000]
+plt.plot(months, revenue)
+plt.title('Monthly Revenue Growth')
+plt.xlabel('Months')
+plt.ylabel('Revenue in USD($)')
+plt.grid()
+plt.show()
